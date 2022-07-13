2049bdf8c69496a194cd520136bca19a867cc947
mateuslustosa <mateuslustosa.o@gmail.com>
2022-07-13 17:37:24 -0300

    Desafio completo

---
+++ script.js
@@ -0,0 +1,17 @@
+let comedia = [{
+    title: 'As Branquelas',
+    diretor: 'Keenen Ivory Wayans',
+    year: 2004
+}]
+
+let action = [{
+    title: 'John Wick: De volta ao jogo',
+    diretor:'Chad Stahelski',
+    year: 2014
+}]
+
+let fantasy = [{
+    title: 'Harry Potter e a Pedra Filosofal',
+    diretor: 'Chris Columbus',
+    year: 2001
+}] 
